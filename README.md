631dfdbcdf2d2ff4fd55399db75af83fb2788706
Christopher Antila <christopher.antila@mail.mcgill.ca>
2012-06-11 17:01:56 -0400

    Updated licences again

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 harrisonHarmony
 ===============
 
-All source code is subject to the GNU GPL 3.0 Licence.
+Copyright Information:
 
-All other content is subject to the CC-BY-SA Unported 3.0 Licence.
+All source code is subject to the GNU GPL 3.0 Licence. A copy of this licence is included as GPL.txt.
+
+All other content is subject to the CC-BY-SA Unported 3.0 Licence. A copy of this licence is included as CC-BY-SA.txt
--- harrisonHarmony.py
+++ harrisonHarmony.py
@@ -4,11 +4,21 @@
 # Name:         harrisonHarmony.py
 # Purpose:      music21-compatible classes for performing and presenting an
 #               analysis in the style of Daniel Harrison (1994)
-#
-# Authors:      Christopher Antila
-#
-# Copyright:    (c) 2012 Christopher Antila
-# License:      GPL v 3.0
+# 
+# Copyright (C) 2012 Christopher Antila
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------------------
 '''
 Classes and functions for performing and presenting a harmonic-functional
